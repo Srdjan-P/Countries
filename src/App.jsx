1079d10cb0b3519acbf89de4fa98de187cacf7a0
Srdjan-P <srdjan.potrebic@gmail.com>
2025-04-25 15:21:10 +0200

    add: scroll up button

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,25 @@ import FormControl from "@mui/material/FormControl";
 import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
 import Loader from "./components/Loader";
+import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
+import Button from "@mui/material/Button";
 
 export default function App() {
   const [countries, setCountries] = useState([]);
   const [input, setInput] = useState("");
   const [region, setRegion] = useState("");
   const [isLoading, setIsLoading] = useState(true);
+  const [scrollPos, setScrollPos] = useState(0);
 
   useEffect(() => {
     getCountries();
+  }, []);
+
+  useEffect(() => {
+    addEventListener("scroll", handleScroll);
+    return () => {
+      removeEventListener("scroll", handleScroll);
+    };
   }, []);
 
   const getCountries = async () => {
@@ -41,72 +51,88 @@ export default function App() {
     }
   });
 
-  return (
-    <div className="wrapper">
-      <div className="main">
-        <div className="search">
-          <TextField
-            id="outlined-basic"
-            variant="outlined"
-            fullWidth
-            label="Search for..."
-            value={input}
-            onChange={(event) => setInput(event.target.value)}
-          />
-        </div>
-        <div>
-          <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Regions</InputLabel>
-              <Select
-                className="select"
-                style={{ width: "200px" }}
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={region}
-                label="Regions"
-                onChange={(event) => setRegion(event.target.value)}
-              >
-                <MenuItem value="Europe">Europe</MenuItem>
-                <MenuItem value="Asia">Asia</MenuItem>
-                <MenuItem value="Africa">Africa</MenuItem>
-                <MenuItem value="Americas">America</MenuItem>
-                <MenuItem value="Oceania">Oceania</MenuItem>
-                <MenuItem value="Antarctic">Antarctic</MenuItem>
-              </Select>
-            </FormControl>
-          </Box>
-        </div>
-      </div>
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPos(position);
+  };
 
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <div className="country-container">
-          {filteredData.map((country) => {
-            return (
-              <div key={country.name} className="country">
-                <img src={country.flag.large} alt={country.name} />
-                <div className="details">
-                  <h4>{country.name}</h4>
-                  <p>
-                    <b>population: </b>
-                    {country.population}
-                  </p>
-                  <p>
-                    <b>Region: </b>
-                    {country.region}
-                  </p>
-                  <p>
-                    <b>Capital: </b>
-                    {country.capital}
-                  </p>
-                </div>
-              </div>
-            );
-          })}
+  return (
+    <>
+      <div className="wrapper">
+        <div className="main">
+          <div className="search">
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              fullWidth
+              label="Search for..."
+              value={input}
+              onChange={(event) => setInput(event.target.value)}
+            />
+          </div>
+          <div>
+            <Box sx={{ minWidth: 120 }}>
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Regions</InputLabel>
+                <Select
+                  className="select"
+                  style={{ width: "200px" }}
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={region}
+                  label="Regions"
+                  onChange={(event) => setRegion(event.target.value)}
+                >
+                  <MenuItem value="Europe">Europe</MenuItem>
+                  <MenuItem value="Asia">Asia</MenuItem>
+                  <MenuItem value="Africa">Africa</MenuItem>
+                  <MenuItem value="Americas">America</MenuItem>
+                  <MenuItem value="Oceania">Oceania</MenuItem>
+                  <MenuItem value="Antarctic">Antarctic</MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+          </div>
         </div>
-      )}
-    </div>
+
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <div className="country-container">
+            {filteredData.map(({ name, population, region, capital, flag }) => {
+              return (
+                <div key={name} className="country">
+                  <img src={flag.large} alt={name} />
+                  <div className="details">
+                    <h4>{name}</h4>
+                    <p>
+                      <b>population: </b>
+                      {population}
+                    </p>
+                    <p>
+                      <b>Region: </b>
+                      {region}
+                    </p>
+                    <p>
+                      <b>Capital: </b>
+                      {capital}
+                    </p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </div>
+      <div className={`arrow ${scrollPos > 300 ? `show` : ""}`}>
+        <Button
+          variant="contained"
+          size="small"
+          onClick={() => scrollTo({ top: 0, behavior: "smooth" })}
+        >
+          <ArrowCircleUpIcon fontSize="large" />
+        </Button>
+      </div>
+    </>
   );
 }
